2ca51560fc69066eed4057f010908650eae858aa
mhavlicek <malia.havlicek@gmail.com>
2022-09-19 19:40:59 -0600

    add in some touch detection stuff

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,8 +4,10 @@ const names_block = document.getElementById('names');
 const colors = ['#eae4e9ff', '#fff1e6ff', '#fde2e4ff', '#fad2e1ff', '#e2ece9ff', '#bee1e6ff', '#f0efebff',
     '#dfe7fdff', '#cddafdff', '#dfd7fcff', '#f8b4c4ff', '#ffedc2ff', '#60fbd2ff', '#7cd5f3ff', '#8fdbf5ff', '#3dccc7ff'
 ];
+
 let group_elements = [];
 let moving_element = '';
+let start_swipe;
 const flight_elements = document.querySelectorAll('.drop-targets');
 
 
@@ -39,6 +41,73 @@ function dragLeave() {
     this.classList.toggle('hovered');
 }
 
+const touchMove = e => {
+
+    //only 1 finger action detected
+    if (e.targetTouches.length == 1) {
+        const progressX = startX - e.touches[0].clientX
+        const translation =
+            progressX > 0
+                ? parseInt(-Math.abs(progressX))
+                : parseInt(Math.abs(progressX))
+        moving_element.style.setProperty('--translate', translation)
+
+        let touch = e.targetTouches[0];
+        const destination_element = document.elementFromPoint(touch.clientX, touch.clientY);
+        if (destination_element && destination_element.classList.contains('drop-targets')) {
+            destination_element.classList.add('hovered');
+        } else {
+            const hovered_elements = document.querySelectorAll('.hovered.drop-targets');
+            try {
+                if (hovered_elements && hovered_elements.length > 0) {
+                    for (const flight_element in hovered_elements) {
+                        flight_element.classList.remove('hovered');
+                    }
+                }
+            }
+            catch(err){
+            // do nothing, probably have a race condition
+            }
+        }
+
+    }
+}
+
+const touchEnd = e => {
+
+    const finishingTouch = e.changedTouches[0]
+
+    moving_element.classList.remove('hold');
+
+    const destination_element = document.elementFromPoint(finishingTouch.clientX, finishingTouch.clientY);
+    if (destination_element && destination_element.classList.contains('drop-targets')) {
+        destination_element.append(moving_element);
+        destination_element.classList.remove('hovered');
+        // need to remove original element
+
+    }
+
+}
+
+const touchStart = e => {
+
+    e.stopPropagation();
+    const {touches} = e
+    // only 1 finger touch is actionable
+    if (touches && touches.length === 1) {
+        const touch = touches[0]
+        startX = touch.clientX;
+        moving_element = e.currentTarget;
+        moving_element.classList.add('hold');
+        moving_element.removeEventListener('touchmove', touchMove);
+        moving_element.removeEventListener('touchend', touchEnd);
+        moving_element.addEventListener('touchmove', touchMove);
+        moving_element.addEventListener('touchend', touchEnd);
+
+    }
+}
+
+
 function dragDrop(e) {
     this.classList.toggle('hovered');
     console.log(e);
@@ -51,6 +120,12 @@ function dragDrop(e) {
     2. if person is moved out of group, re cal group weight
     3. when group or person put into a flight, recalc balances
      */
+
+    // when names is empty, show the names-empty div
+    if (document.querySelectorAll('#names .person').length == 0) {
+        document.querySelector('.names-empty').classList.toggle('hide');
+        document.getElementById('names').classList.add('hide');
+    }
 
 
 }
@@ -76,12 +151,14 @@ function applyGroupHandlers() {
     for (const group_element of group_elements) {
         group_element.removeEventListener('dragstart', dragStart);
         group_element.removeEventListener('dragend', dragEnd);
+        group_element.removeEventListener('touchstart', touchStart);
     }
 
     // apply  group/name listeners
     for (const group_element of group_elements) {
         group_element.addEventListener('dragstart', dragStart);
         group_element.addEventListener('dragend', dragEnd);
+        group_element.addEventListener('touchstart', touchStart);
     }
 }
 
@@ -97,6 +174,9 @@ document.getElementById('import').addEventListener('click', (e) => {
     }
 
     let fr = new FileReader();
+
+    document.querySelector('.names-empty').classList.add('hide');
+    document.getElementById('names').classList.remove('hide');
 
     fr.onload = function (e) {
 
