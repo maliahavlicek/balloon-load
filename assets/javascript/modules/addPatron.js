14932849eebd9d74b4b4a89cdd909cedeab142a0
mhavlicek <malia.havlicek@gmail.com>
2022-09-26 11:05:05 -0600

    clean up handlers after changes

--- assets/javascript/modules/addPatron.js
+++ assets/javascript/modules/addPatron.js
@@ -26,7 +26,7 @@ function addPatron() {
     const group_no = document.querySelectorAll('.group:not(.person):not(.guest)').length + parseInt(guests_added) + 1;
 
 
-    let new_element = `<div class="group guest group-${group_no} d-flex flex-column" draggable="true" style="--translateX:0; --translateY:0;">` +
+    let new_element = `<div class="group guest group-${group_no} d-flex flex-column" draggable="true" data-group_number="${group_no}"style="--translateX:0; --translateY:0;" draggable="true" >` +
         `<div class="group-name" data-group-name="${name}">` +
         `<span class="count">1</span>` +
         `<span>-</span>` +
@@ -35,7 +35,7 @@ function addPatron() {
         `<div class="d-flex flex-row">` +
         `<div class="weight group-weight">${weight}</div>` +
         `<div class="details d-block">` +
-        `<div class="person group group-${group_no} d-flex flex-row" data-weight="${weight}" data-count="1" data-name="${name}" draggable="true" style="--translateX:0;--translateY:0;">` +
+        `<div class="person group group-${group_no} d-flex flex-row" data-weight="${weight}" data-count="1" data-group_number="${group_no}" data-name="${name}" style="--translateX:0;--translateY:0;">` +
         `<div class="weight">${weight}</div>` +
         `</div></div></div></div>`;
     document.getElementById('names').insertAdjacentHTML('beforeend', new_element);
